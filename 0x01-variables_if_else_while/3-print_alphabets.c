52fa0c238f1b3811ba725eec058dbddc93a9b6fb
CasualCartoon <muiruriian4@gmail.com>
2023-09-12 12:02:16 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char c;
+
+	char d;
+
+	c = 'a';
+	d = 'A';
+	while
+		(c <= '2') {
+			putchar(c);
+			c++;
+		}
+	while
+		(d <= '2') {
+			putchar (d);
+			d++;
+
+		}
+	putchar('\n');
+	return (0);
+}
