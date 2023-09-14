e1fb0bcea4ce4dab83698a8c9658c3defb20e498
CasualCartoon <muiruriian4@gmail.com>
2023-09-14 05:37:53 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -4,39 +4,33 @@
  * main - Entry point
  *
  * Description: Prints all possible combinations of two two-digit numbers
- * Numbers range from 0 to 99 and are printed with two digits.
- * Combinations are separated by a comma followed by a space.
+ * separated by a space, with each number printed in ascending order.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int tens1, ones1, tens2, ones2;
+	int num1, num2, num3, num4;
 
-	for (tens1 = 0; tens1 < 10; tens1++)
+	for (num1 = 0; num1 <= 9; num1++)
 	{
-		for (ones1 = 0; ones1 < 10; ones1++)
+		for (num2 = 0; num2 <= 9; num2++)
 		{
-			for (tens2 = tens1; tens2 < 10; tens2++)
+			for (num3 = num1; num3 <= 9; num3++)
 			{
-				/* Start ones2 from ones1 if tens2 is the same as tens1 */
-				int start_ones2 = (tens2 == tens1) ? ones1 : 0;
+				/* Start num4 from num1 if num3 is the same as num1 */
+				int start = (num3 == num1) ? num2 + 1 : 0;
 
-				for (ones2 = start_ones2; ones2 < 10; ones2++)
+				for (num4 = start; num4 <= 9; num4++)
 				{
-					/* Print the first two-digit number */
-					putchar('0' + tens1);
-					putchar('0' + ones1);
-
-					/* Print a space to separate the numbers */
+					putchar(num1 + '0');
+					putchar(num2 + '0');
 					putchar(' ');
-
-					/* Print the second two-digit number */
-					putchar('0' + tens2);
-					putchar('0' + ones2);
+					putchar(num3 + '0');
+					putchar(num4 + '0');
 
 					/* Check if it's not the last combination */
-					if (!(tens1 == 9 && ones1 == 9 && tens2 == 9 && ones2 == 9))
+					if (!(num1 == 9 && num2 == 8 && num3 == 9 && num4 == 9))
 					{
 						putchar(',');
 						putchar(' ');
